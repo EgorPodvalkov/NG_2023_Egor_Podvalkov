740281a59d5475875cec2d7e8183c13d7bcc0926
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-03-29 22:46:41 +0300

    Added Task1 to lesson4

---
+++ Lesson4/Task1/Customer.cs
@@ -0,0 +1,46 @@
+﻿namespace Task1;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Balance { get; set; }
+
+    public Customer(int id, string name, decimal balance)
+    {
+        Id = id;
+        Name = name;
+        Balance = balance;
+    }
+
+    public static List<Customer> CustomersList = new List<Customer>()
+    {
+        new Customer(1, "Fikus", 0),
+        new Customer(2, "VHarbar", 100000)
+    };
+
+    public static void PrintBalanceToConsole(int id)
+        => Console.WriteLine($"Customer`s with id {id} balance is: {GetBalanceById(id)}");
+
+    public static decimal GetBalanceById(int id)
+    {
+        var customer = GetCustomerById(id);
+        if (customer == null)
+            return -1;
+        return customer.Balance;
+    }
+
+    public static Customer? GetCustomerById(int id)
+        => CustomersList.FirstOrDefault(x => x.Id == id);
+    
+    public static void UpdateBalance(int id, decimal newBalance)
+    {
+        var customer = GetCustomerById(id);
+        if (customer == null)
+            return;
+        customer.Balance = newBalance;
+        customer.SaveToDatabase();
+    }
+
+    private void SaveToDatabase() => Console.WriteLine("Saved!");
+}
---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Task1;
+
+var c = new Customer(4,"4",132);
+
+var list = Customer.CustomersList;
+
+foreach (var item in list)
+    Console.WriteLine($"{item.Id}, {item.Name}, {item.Balance}$");
+
+c.UpdateBalance(1, 23);
+
+foreach (var item in list)
+    Console.WriteLine($"{item.Id}, {item.Name}, {item.Balance}$");
+
+Console.WriteLine("Hello, World!");
