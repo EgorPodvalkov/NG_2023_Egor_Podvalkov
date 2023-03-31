2053a6bf7555623c2ca59c3267fee50bcc437a3a
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-03-31 11:03:41 +0300

    Added Task2 to Lesson4

---
+++ Lesson4/Task2/MagicClass.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2;
+
+public class MagicClass
+{
+    public virtual void CountYourMagic(int magic)
+        => Console.WriteLine("I understand you...");
+}
+
+public class FireMagic : MagicClass
+{
+    public override void CountYourMagic(int magic)
+    {
+        if (magic == 150)
+            Console.WriteLine("Wow, your magic is fire magic!");
+        else
+            base.CountYourMagic(magic);
+    }
+}
+
+public class WaterMagic : MagicClass
+{
+    public override void CountYourMagic(int magic)
+    {
+        if (magic == 50000000)
+            Console.WriteLine("Incredible! You have 50 millions of power! It's water magic!");
+        else
+            base.CountYourMagic(magic);
+    }
+}
+
