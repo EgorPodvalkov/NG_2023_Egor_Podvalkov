5abe641e1ab4507c6b1959a3af161cc67a9ad439
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-04-04 10:58:24 +0300

    Added CustomerManager class to Task1, Lesson4

--- Lesson4/Task1/Customer.cs
+++ Lesson4/Task1/Customer.cs
@@ -14,26 +14,3 @@ public class Customer
         CustomerManager.CustomersList.Add(this);
     }
 }
-
-public static class CustomerManager
-{
-    public static List<Customer> CustomersList = new List<Customer>()
-    {
-        new Customer(1, "Fikus", 0),
-        new Customer(2, "VHarbar", 100000)
-    };
-
-    public static void PrintBalanceToConsole(int id)
-        => Console.WriteLine($"Customer`s with id {id} balance is: {GetBalanceById(id)}");
-
-    public static decimal GetBalanceById(int id)
-    {
-        var customer = GetCustomerById(id);
-        if (customer == null)
-            return -1;
-        return customer.Balance;
-    }
-
-    public static Customer? GetCustomerById(int id)
-        => CustomersList.FirstOrDefault(x => x.Id == id);
-}
---
+++ Lesson4/Task1/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1;
+
+public static class CustomerManager
+{
+    public static List<Customer> CustomersList = new List<Customer>()
+    {
+        new Customer(1, "Fikus", 0),
+        new Customer(2, "VHarbar", 100000)
+    };
+
+    public static void PrintBalanceToConsole(int id)
+        => Console.WriteLine($"Customer`s with id {id} balance is: {GetBalanceById(id)}");
+
+    public static decimal GetBalanceById(int id)
+    {
+        var customer = GetCustomerById(id);
+        if (customer == null)
+            return -1;
+        return customer.Balance;
+    }
+
+    public static Customer? GetCustomerById(int id)
+        => CustomersList.FirstOrDefault(x => x.Id == id);
+}
