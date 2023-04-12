512c925183706c5cd076f9cb6264ca8ed2d883d2
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-04-12 12:22:54 +0300

    Simplified FilesManager class in Task3, Lesson4

--- Lesson4/Task3/FilesManager.cs
+++ Lesson4/Task3/FilesManager.cs
@@ -4,44 +4,45 @@ namespace Task3;
 
 public class FilesManager : IEditingFile, IReadingFromFile
 {
+    private IUserChecker _check = new UsersManager();
     public string ReadFromFile(string filename, Guid role)
     {
-        var checkedRole = new UsersManager().CheckRole(role);
+        _check.CheckRole(role);
         return "Readed Data";
     }
     public void WriteToFile(string filename, Guid role)
     {
-        var checkedRole = new UsersManager().CheckRole(role);
+        _check.CheckRole(role);
         Console.WriteLine("Data Written");
     }
     public void DeleteFile(string filename, Guid role)
     {
-        var checkedRole = new UsersManager().CheckRole(role);
+        _check.CheckRole(role);
         Console.WriteLine("File Deleted");
     }
     public void DownloadFile(string filename, Guid role)
     {
-        var checkedRole = new UsersManager().CheckRole(role);
+        _check.CheckRole(role);
         Console.WriteLine("File Downloaded");
     }
     public void CopyFile(string filename, Guid role)
     {
-        var checkedRole = new UsersManager().CheckRole(role);
+        _check.CheckRole(role);
         Console.WriteLine("File Copied");
     }
     public void GetDataFromFile(string filename, Guid role)
     {
-        var checkedRole = new UsersManager().CheckRole(role);
+        _check.CheckRole(role);
         Console.WriteLine("Data Get");
     }
     public void CheckFile(string filename, Guid role)
     {
-        var checkedRole = new UsersManager().CheckRole(role);
+        _check.CheckRole(role);
         Console.WriteLine("File Checked");
     }
     public void SaveToFile(string filename, Guid role)
     {
-        var checkedRole = new UsersManager().CheckRole(role);
+        _check.CheckRole(role);
         Console.WriteLine("File Saved");
     }
 }
