4fe3725d1e30b05d06344ac7fa111ac7022f7113
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-04-12 13:55:15 +0300

    Added Task1 to Lesson6

---
+++ Lesson6/Task1/Calculator.cs
@@ -0,0 +1,32 @@
+﻿namespace Task1;
+
+public class Calculator
+{
+    public delegate float Operation(float num1, float num2);
+    public delegate void CalculateHandler(string text);
+    public event CalculateHandler? Log;
+    private Dictionary<Operation, string> _operationSymbol = new Dictionary<Operation, string>()
+        {
+            { Add, "+" },
+            { Sub, "-" },
+            { Mul, "*" },
+            { Div, "/" }
+        };
+
+    public Calculator() { }
+
+    public float Calculate(float num1, float num2, Operation operation)
+    {
+        var result = operation(num1, num2);
+
+        // Examples: "1 + 2 = 3", "3 * 4 = 12"
+        Log?.Invoke($"{num1} {_operationSymbol[operation]} {num2} = {result}");
+        
+        return result;
+    }
+
+    public static float Add(float num1, float num2) => num1 + num2;
+    public static float Sub(float num1, float num2) => num1 - num2;
+    public static float Mul(float num1, float num2) => num1 * num2;
+    public static float Div(float num1, float num2) => num1 / num2;
+}
---
+++ Lesson6/Task1/UI.cs
@@ -0,0 +1,84 @@
+﻿namespace Task1;
+
+public class UI
+{
+    private Calculator _calculator = new Calculator();
+
+    public UI(bool run = false)
+    {
+        _calculator.Log += Console.WriteLine;
+        while(run)
+            Run();
+    }
+
+    public void Run()
+    {
+        // Clearing
+        Console.Clear();
+
+        // Getting Arguments
+        var num1 = GetNumberFromConsole("Enter First Number: ");
+        var operation = GetOperationFromConsole();
+        var num2 = GetNumberFromConsole("Enter Second Number: ");
+        Console.WriteLine();
+        
+        // Calculating
+        _calculator.Calculate(num1, num2, operation);
+
+        // Waiting User
+        Console.ReadLine();
+    }
+
+    private float GetNumberFromConsole(string message = "Enter Number: ")
+    {
+        // Asking Number
+        Console.Write(message);
+        float result;
+
+        // Getting Number
+        while (!float.TryParse(Console.ReadLine(), out result))
+        {
+            // Asking Number
+            Console.WriteLine("Something Wrong :(, Try Again!");
+            Console.Write(message);
+        }
+        
+        return result;
+    }
+
+    private Calculator.Operation GetOperationFromConsole(string message = "Enter Operation: ")
+    {
+        Calculator.Operation? operation = null;
+
+        while (operation == null)
+        {
+            // Asking Operation
+            Console.Write(message);
+
+            // Getting User Response
+            var response = Console.ReadLine().ToLower();
+
+            // Getting Operation Add
+            if (response == "+" || response == "sum" || response == "add" || response == "addition")
+                operation = Calculator.Add;
+
+            // Getting Operation Sub
+            else if (response == "-" || response == "sub" || response == "subtract" || response == "subtraction")
+                operation = Calculator.Sub;
+
+            // Getting Operation Mul
+            else if (response == "*" || response == "mul" || response == "times" || response == "multiply")
+                operation = Calculator.Mul;
+
+            // Getting Operation Div
+            else if (response == "/" || response == "div" || response == "division" || response == "divide")
+                operation = Calculator.Div;
+
+            // if Bad Operation
+            else
+                Console.WriteLine("Something Wrong :(, Try Again!");
+        }
+
+        return operation;
+    }
+}
